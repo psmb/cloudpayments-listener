92606a18c8c751232b7048dbf2b9203ac4e8d44a
Dmitri Pisarev <dimaip@gmail.com>
2019-12-16 12:00:48 +0300

    Add furniture project

--- config.js
+++ config.js
@@ -9,6 +9,7 @@ const kabinetHostname = process.env.KABINET_HOST || 'https://kabinet.sfi.ru';
 const hmacKey = process.env.HMAC_KEY;
 const hmacKeys = {
     projects: process.env.HMAC_KEY_PROJECTS,
+    furniture: process.env.HMAC_KEY_FURNITURE,
     social: process.env.HMAC_KEY_SOCIAL
 };
 
